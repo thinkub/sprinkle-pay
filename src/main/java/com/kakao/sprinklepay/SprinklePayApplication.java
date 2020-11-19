7dbc0452e40e3893d48e9006d44e58d817a48cd6
mhlee <thinkub0219@gmail.com>
2020-11-19 15:37:25 +0900

    JPA AuditingEntityListener 를 사용하기 위한 설정 추가

--- src/main/java/com/kakao/sprinklepay/SprinklePayApplication.java
+++ src/main/java/com/kakao/sprinklepay/SprinklePayApplication.java
@@ -2,8 +2,10 @@ package com.kakao.sprinklepay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SprinklePayApplication {
 
     public static void main(String[] args) {
