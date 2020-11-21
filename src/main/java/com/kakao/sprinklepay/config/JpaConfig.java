618a5c6c49b907719de72adf61b495c8e0793b65
mhlee <thinkub0219@gmail.com>
2020-11-21 14:51:12 +0900

    EnableJpaAuditing 을 config로 빼지 않으면 testCase 에 문제가 발생하여 별도 Configuration 으로 분리

---
+++ src/main/java/com/kakao/sprinklepay/config/JpaConfig.java
@@ -0,0 +1,13 @@
+package com.kakao.sprinklepay.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @author by Ming(thinkub0219@gmail.com) on 2020/11/21.
+ */
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
